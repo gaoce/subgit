46b2143c44fa1cb195b1103cd28f9614f12308b2
gaoce <gaoce@coe.neu.edu>
2015-04-19 22:33:26 -0400

    Add another soln using git

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Download a sub folder from github.com
 
 ## Reference
 Based on [this
-question](http://stackoverflow.com/questions/7106012/download-a-single-folder-or-directory-from-a-github-repo)
+question](http://stackoverflow.com/questions/7106012/download-a-single-folder-or-directory-from-a-github-repo) and [this](http://stackoverflow.com/questions/2466735/how-to-checkout-only-one-file-from-git-repository/2466755#2466755)
+
--- subgit.py
+++ subgit.py
@@ -1,26 +1,63 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
+import re
 import argparse
 import subprocess as sp
+import sys
+import os.path
 
 
 def parse_args():
     description = "Download a sub folder in a github repository"
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('link', help='Github link for svn')
-    parser.add_argument('sub', help='Sub folder in the repository')
+    parser.add_argument('path', help='File or folder to download')
     parser.add_argument('--branch', default='master',
                         help='Branch in the repository')
+    parser.add_argument('--software', default='git', choices=['git', 'svn'],
+                        help='Software to be used, either git or svn')
     # TODO set new folder name
     return parser.parse_args()
 
 
-def download(args):
+def download_git(args):
+
+    # TODO check git availability
+    try:
+        sp.check_call(['git', 'clone', '-n', '--depth', '1', args.link])
+    except sp.CalledProcessError:
+        print('Sorry the folder cannot be downloaded', file=sys.stderr)
+        sys.exit(1)
+
+    # Get repository name and cd into it
+    repo_name = re.search('(.*?)\.git', os.path.basename(args.link)).group(1)
+    os.chdir(repo_name)
+
+    # Check out the desired path
+    try:
+        sp.check_call(['git', 'checkout', args.branch, args.path])
+    except sp.CalledProcessError:
+        print('The path is not available in the repo', file=sys.stderr)
+        sys.exit(1)
+
+    # Get out of here the repository
+    os.chdir('..')
+
+
+def download_svn(args):
     if args.branch == 'master':
         branch = 'trunk'
     else:
         branch = args.branch
 
-    sub_path = '{}/{}/{}'.format(args.link, branch, args.sub)
+    # Convert SSH or HTTPS link to svn link
+    link = args.link.rstrip('.git')
+    if link.startswith('git'):
+        link = link.replace(':', '/')
+        link = link.replace('git@', 'https://')
+
+    sub_path = '{}/{}/{}'.format(link, branch, args.path)
 
     # TODO check svn availability
     try:
@@ -31,7 +68,12 @@ def download(args):
 
 def main():
     args = parse_args()
-    download(args)
+    if args.software == 'git':
+        download_git(args)
+    elif args.software == 'svn':
+        download_svn(args)
+    else:
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()

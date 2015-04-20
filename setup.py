a32a5037952fe085b19578b1355b46983a1226cc
gaoce <gaoce@coe.neu.edu>
2015-04-20 12:29:47 -0400

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="subgit",
+    version="0.1",
+    author="Ce Gao",
+    author_email="andrewjcarter@gmail.com",
+    description=("Download a sub folder from github.com"),
+    license="MIT",
+    packages=['subgit'],
+    long_description=read('README.md'),
+    entry_points={'console_scripts': ['subgit = subgit.subgit:main']}
+)
